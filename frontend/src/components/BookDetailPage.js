3c3e6820619fd7aa57584ba559e959b77f3f2094
anny-tar <2201236@stud.samgups.ru>
2025-05-17 18:00:03 +0400

    Создание компонента для отображения детальной информации о книге

---
+++ frontend/src/components/BookDetailPage.js
@@ -0,0 +1,8 @@
+// frontend/src/components/BookDetailPage.js
+import React from 'react';
+
+const BookDetailPage = () => {
+  return <div>Страница деталей книги</div>;
+};
+
+export default BookDetailPage;
