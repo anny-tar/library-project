cde102336fd8562d9f42e8bf19ca2faadfbd3032
anny-tar <2201236@stud.samgups.ru>
2025-05-17 17:57:15 +0400

    Изменение маршрутов проекта

--- backend/library/urls.py
+++ backend/library/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from books import views
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router = routers.DefaultRouter()
 router.register(r'authors', views.AuthorViewSet, basename='author')
@@ -13,5 +17,7 @@ router.register(r'reviews', views.ReviewViewSet, basename='review')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include('books.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
