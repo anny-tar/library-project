ff7f95081bd4c95de65fc1c15405313d37fd067f
anny-tar <2201236@stud.samgups.ru>
2025-05-17 17:55:21 +0400

    Изменение точки входа в приложение

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const App = () => <h1>Library Frontend Works!</h1>;
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
