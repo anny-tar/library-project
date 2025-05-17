09dfd0814a9dba50ccaefbd536fee7c1e4139d6c
anny-tar <2201236@stud.samgups.ru>
2025-05-17 18:00:37 +0400

    Создание компонента для панели управления

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,9 @@
+// frontend/src/components/Dashboard.js
+import React from 'react';
+
+const Dashboard = () => {
+  return <div>Dashboard Content</div>;
+};
+
+// Важно! Добавьте экспорт
+export default Dashboard;
